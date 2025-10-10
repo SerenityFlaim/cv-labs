a20fa489f96d7fe47e5fc111aa068b65b9ec9cd0
Philipp Matuha <phil_th@mail.ru>
2025-10-10 16:09:25 +0300

    L3 T2 normalized kernels

--- lab1/lab3/gauss_blur.py
+++ lab1/lab3/gauss_blur.py
@@ -1,8 +1,10 @@
 import cv2 as cv
 import numpy as np
 
+SQUARE_DEVIATION = 5
+
 def form_gauss_kernel(n, sq_deviation):
-    ker = [[0.0 for _ in range(n)] for _ in range(n)]
+    ker = np.array([[0.0 for _ in range(n)] for _ in range(n)])
     a = n - n // 2
     b = a
     for i in range(n):
@@ -13,5 +15,21 @@ def form_gauss_kernel(n, sq_deviation):
     
     return ker
 
-ker = form_gauss_kernel(3, 5)
-print(ker)
+
+
+kernel3 = form_gauss_kernel(3, SQUARE_DEVIATION)
+kernel5 = form_gauss_kernel(5, SQUARE_DEVIATION)
+kernel7 = form_gauss_kernel(7, SQUARE_DEVIATION)
+
+# print("KER 3\n", kernel3)
+# print("KER 5\n", kernel5)
+# print("KER 7\n", kernel7)
+
+
+kernel3 = kernel3 / kernel3.sum()
+kernel5 = kernel5 / kernel5.sum()
+kernel7 = kernel7 / kernel7.sum()
+
+print("Normalized_3 - Sum: ", kernel3.sum(), "\n", kernel3)
+print("Normalized_5 - Sum: ", kernel5.sum(), "\n", kernel5)
+print("Normalized_7 - Sum: ", kernel7.sum(), "\n", kernel7)
