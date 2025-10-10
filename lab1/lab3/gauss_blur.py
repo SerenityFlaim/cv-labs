9db0628a90fd6537ace9b43a6304653f24100ca6
Philipp Matuha <phil_th@mail.ru>
2025-10-10 19:26:18 +0300

    L3 T3 blur working

--- lab1/lab3/gauss_blur.py
+++ lab1/lab3/gauss_blur.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 
+MX_DIMENSION = 5
 SQUARE_DEVIATION = 5
 
 def form_gauss_kernel(n, sq_deviation):
@@ -33,3 +34,23 @@ kernel7 = kernel7 / kernel7.sum()
 print("Normalized_3 - Sum: ", kernel3.sum(), "\n", kernel3)
 print("Normalized_5 - Sum: ", kernel5.sum(), "\n", kernel5)
 print("Normalized_7 - Sum: ", kernel7.sum(), "\n", kernel7)
+
+image = cv.imread("/home/serenity-flaim/Desktop/CV/lab1/media/lab3/sonic_wisdom.jpg")
+grayscale_img = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+cv.imwrite("/home/serenity-flaim/Desktop/CV/lab1/media/lab3/grayscale.jpg", grayscale_img)
+
+h, w = grayscale_img.shape[:2]
+blur_image = np.array([[0.0 for _ in range(w)] for _ in range(h)])
+
+padding = MX_DIMENSION // 2
+padded_img = np.pad(grayscale_img, padding, mode='constant', constant_values=0)
+
+
+for i in range(h):
+    for j in range(w):
+        process_region = padded_img[i:i+MX_DIMENSION, j:j+MX_DIMENSION]
+        val = np.sum(process_region * kernel5)
+        blur_image[i, j] = val
+
+blur_image = np.clip(blur_image, 0, 255).astype(np.uint8)
+cv.imwrite("/home/serenity-flaim/Desktop/CV/lab1/media/lab3/gaussian_blur.jpg", blur_image)
